c96b76956d5c64a6aa65c137c557a596d528f212
Sudit Kumar Jha <jsudit2@gmail.com>
2024-02-16 08:07:43 +0000

    Basic functions complete till payment checkout

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import { menuArray, orderArray} from "./data.js"
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+// import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 // console.log(menuArray)
 // console.log(uuidv4())
 
 const menuList = document.getElementById("menu-list")
 const orderList = document.getElementById('order-list-container')
 
-menuList.addEventListener("click", handleAddItemEvent)
-orderList.addEventListener("click", handleRemoveItemEvent)
+
+menuList.addEventListener("click", handleAddItemEvent)//Add Item to menu
+orderList.addEventListener("click", handleRemoveItemOrCompleteOrderEvent)//Remove Item from Orders
 
 // Event Listeners
 
@@ -15,27 +16,32 @@ function handleAddItemEvent(e){
     const menuItem = menuArray.filter((menuItem) => {
         return menuItem.uuid === e.target.id
     })[0]
-    if(!orderArray.includes(menuItem)){
+    if(!orderArray.includes(menuItem) && menuItem){
         orderArray.push(menuItem)
         renderOrders(orderArray)
     }
     // console.log(orderArray)
 }
 
-function handleRemoveItemEvent(e){
-    // console.log(e.target.id)
+function handleRemoveItemOrCompleteOrderEvent(e){
+    
     const removedItem = orderArray.filter((orderItem) => {
         return orderItem.uuid === e.target.id
     })[0]
-    const removedItemIndex = orderArray.indexOf(removedItem)
-    // console.log(removedItem)
-    // console.log(removedItemIndex)
-    orderArray.splice(removedItemIndex, 1)
-    // console.log(orderArray)
-    renderOrders(orderArray)
+    if(removedItem){
+        const removedItemIndex = orderArray.indexOf(removedItem)
+        orderArray.splice(removedItemIndex, 1)
+        renderOrders(orderArray)
+    }
+// Handle Complete Order Event
+    if(e.target.id === 'complete-order-btn'){
+        // console.log(e.target.id)
+        document.getElementById("payment-modal").style.display = 'block'
+        completeOrderPayment()
+    }
 }
 
-// Functions
+// Function Calls from Event Listeners
 
 // Rendering Orders Array
 function renderOrders(ordersArray){
@@ -55,9 +61,38 @@ function renderOrders(ordersArray){
     }else{
         orderList.innerHTML = ''
     }
-    
-
 }
+
+function completeOrderPayment(){
+    const paymentFormEl = document.getElementById('payment-modal-inner')
+
+    document.getElementById("customer-name").addEventListener("keypress", function(evt) {
+        const keyPressed = evt.key;
+        const isNumber = /^\d$/.test(keyPressed);
+        if (isNumber) {
+            evt.preventDefault();
+        }
+    })
+
+    document.getElementById("cvv").addEventListener("keypress", function(evt) {
+        const keyPressed = evt.key;
+        const isNumber = /^\d$/.test(keyPressed);
+        if (!isNumber) {
+            evt.preventDefault();
+        }
+    })
+    
+    paymentFormEl.addEventListener("submit", function(e){
+        e.preventDefault()
+        const formData = new FormData(paymentFormEl)
+        orderComplete(formData.get('customer-name'))
+        paymentFormEl.reset()
+    })
+    // console.log(paymentFormEl)
+    
+}
+
+// Function Calls from Other Functions
 
 function getOrderItemListHtml(ordersArray){
     return ordersArray.map((item) => {
@@ -77,6 +112,28 @@ function getOrderTotal(orderArray){
     return orderArray.reduce((total, item) => total + item.price, 0)
 }
 
+
+function orderComplete(name){
+    document.getElementById("payment-modal").style.display = 'none'
+    orderList.style.display = 'none'
+
+    document.getElementById("order-complete").innerHTML = `
+        <div class="order-complete">
+            <h2>Thanks, ${name}! Your order is on its way!</h2>
+        </div>
+    `
+    setTimeout(function(){
+        document.getElementById("order-complete").innerHTML = ''
+        let orderArrayLength = orderArray.length 
+        for(let i = 0; i < orderArrayLength; i++){
+            orderArray.pop()
+        }
+        
+    }, 5000)
+    console.log(name)
+}
+
+// Default Menu Function Called on Load
 function renderMenu(menuArray){
     return menuArray.map((item)=>{
         return `
